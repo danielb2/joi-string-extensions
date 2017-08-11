398b278794d73b6019f7bc043f1ea16e6c4b9e3e
Daniel Bretoi <daniel@bretoi.com>
2017-08-10 19:48:27 -0700

    initial commit

---
+++ test/index.js
@@ -0,0 +1,39 @@
+'use strict';
+const Code = require('code');
+const BaseJoi = require('joi');
+const Lab = require('lab');
+
+const Extend = require('../');
+const Joi = BaseJoi.extend(Extend);
+
+// Test shortcuts
+
+const lab = exports.lab = Lab.script();
+const describe = lab.describe;
+const it = lab.it;
+const expect = Code.expect;
+
+
+describe('strings', () => {
+
+    it('should convert to camelCase', (done) => {
+
+        const res = Joi.attempt('hello world', Joi.string().camelCase());
+        expect(res).to.equal('helloWorld');
+        done();
+    });
+
+    it('should convert to CamelCase', (done) => {
+
+        const res = Joi.attempt('hello world', Joi.string().CamelCase());
+        expect(res).to.equal('HelloWorld');
+        done();
+    });
+
+    it('should do nothing', (done) => {
+
+        const res = Joi.attempt('hello world', Joi.string());
+        expect(res).to.equal('hello world');
+        done();
+    });
+});
